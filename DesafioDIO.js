ce1881670914a2ba6058fd6ded092d5237fdfd26
Sofia Helene <sofiahelene1403@gmail.com>
2025-03-17 10:39:17 -0300

    final version

--- DesafioDIO.js
+++ DesafioDIO.js
@@ -1,21 +1,28 @@
 // Desafio classificador de nível de Herói
-let nomeHeroi = "Jefin";
-let xp = 5786;
+let nomeHeroi = ["Jefin", "Maikon", "Aspas", "DGGames"];
+let xp = [3504, 4567, 100000, 678];
 
-if (xp < 1000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Ferro com ${xp} de XP`);
-} else if (xp >= 1000 && xp < 2000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Bronze com ${xp} de XP`);
-} else if (xp >= 2000 && xp < 5000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Prata com ${xp} de XP`);
-} else if (xp >= 5000 && xp < 7000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Ouro com ${xp} de XP`);
-} else if (xp >= 7000 && xp < 8000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Platina com ${xp} de XP`);
-} else if (xp >= 8000 && xp < 9000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Ascendente com ${xp} de XP`);
-} else if (xp >= 9000 && xp < 10000) {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Imortal com ${xp} de XP`);
-} else {
-    console.log(`O herói de nome ${nomeHeroi} está no nível Radiante com ${xp} de XP`);
+for ( i = 0; i < nomeHeroi.length; i++) {
+    let nivel;
+    
+    if (xp[i] < 1000) {
+        nivel = "Ferro";
+    } else if (xp[i] < 2000) {
+        nivel = "Bronze";
+    } else if (xp[i] < 5000) {
+        nivel = "Prata";
+    } else if (xp[i] < 7000) {
+        nivel = "Ouro";
+    } else if (xp[i] < 8000) {
+        nivel = "Platina";
+    } else if (xp[i] < 9000) {
+        nivel = "Ascendente";
+    } else if (xp[i] < 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O herói de nome ${nomeHeroi[i]} está no nível ${nivel} com ${xp[i]} de XP.`);
 }
+
